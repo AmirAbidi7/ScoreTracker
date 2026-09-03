dc06f82dd6fba98ea3d91669feae64a74639e1e3
AmirAbidi7 <abidiamir7@gmail.com>
2026-09-03 07:07:01 +0100

    fix: Added a page indicator to the onboarding page

--- mobile/lib/core/configs/app_theme.dart
+++ mobile/lib/core/configs/app_theme.dart
@@ -4,7 +4,7 @@ import 'package:mobile/core/configs/app_colors.dart';
 class AppTheme {
   static final darkTheme = ThemeData(
     fontFamily: 'Pixeloid',
-    primaryColor: AppColors.textDarkColor,
+    primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.darkBackground,
     brightness: .dark,
     inputDecorationTheme: InputDecorationThemeData(
@@ -20,7 +20,7 @@ class AppTheme {
       ),
       enabledBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(0),
-        borderSide: const BorderSide(color: AppColors.shadow, width: 0.4),
+        borderSide: const BorderSide(color: AppColors.secondary, width: 0.4),
       ),
       hintStyle: const TextStyle(
         fontFamily: 'Pixeloid',
@@ -30,15 +30,15 @@ class AppTheme {
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.darkBackground,
-        foregroundColor: AppColors.textDarkColor,
+        foregroundColor: AppColors.primary,
         textStyle: const TextStyle(
-          color: AppColors.textDarkColor,
+          color: AppColors.primary,
           fontFamily: 'Pixeloid',
           fontWeight: .bold,
         ),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadiusGeometry.circular(0),
-          side: const BorderSide(color: AppColors.shadow, width: 0.8),
+          side: const BorderSide(color: AppColors.secondary, width: 0.8),
         ),
       ),
     ),
--- mobile/lib/presentation/onboarding/pages/onboarding.dart
+++ mobile/lib/presentation/onboarding/pages/onboarding.dart
@@ -3,11 +3,24 @@ import 'package:mobile/core/configs/app_colors.dart';
 import 'package:mobile/core/configs/app_images.dart';
 import 'package:mobile/presentation/root/root.dart';
 
-class OnboardingPage extends StatelessWidget {
-  new({super.key});
+class OnboardingPage extends StatefulWidget {
+  const new({super.key});
 
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
   final PageController _pageController = PageController();
-  final List<Map<String, String>> pagesInfo = [
+
+  int _activePage = 0;
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  final List<Map<String, String>> _pagesInfo = [
     {
       "title": "Fan of board games or party games?",
       "hook": "Tired of your friends constantly asking you for the score?",
@@ -22,17 +35,36 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView.builder(
-        controller: _pageController,
-        itemCount: AppImages.onboardingImage.length,
-        itemBuilder: (BuildContext context, int index) {
-          return _page(
-            context,
-            index,
-            pagesInfo[index]['title']!,
-            pagesInfo[index]['hook']!,
-          );
-        },
+      body: Stack(
+        children: [
+          PageView.builder(
+            controller: _pageController,
+            onPageChanged: (int page) {
+              setState(() {
+                _activePage = page;
+              });
+            },
+            itemCount: AppImages.onboardingImage.length,
+            itemBuilder: (BuildContext context, int index) {
+              return _page(
+                context,
+                index,
+                _pagesInfo[index]['title']!,
+                _pagesInfo[index]['hook']!,
+              );
+            },
+          ),
+          Positioned(
+            bottom: 80,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: .center,
+              spacing: 8,
+              children: _indictators(context),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -54,23 +86,23 @@ class OnboardingPage extends StatelessWidget {
         ),
         Container(color: Colors.black.withAlpha(100)),
         SafeArea(
-          child: Column(
-            mainAxisAlignment: .center,
-            crossAxisAlignment: .center,
-            children: [
-              Text(
-                title,
-                textAlign: .center,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: .bold,
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: .center,
+              crossAxisAlignment: .center,
+              children: [
+                Text(
+                  title,
+                  textAlign: .center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: .bold,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 32),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                child: Text(
+                const SizedBox(height: 64),
+                Text(
                   hook,
                   textAlign: .center,
                   style: const TextStyle(
@@ -79,25 +111,46 @@ class OnboardingPage extends StatelessWidget {
                     fontWeight: .w500,
                   ),
                 ),
-              ),
-              const SizedBox(height: 32),
-              if (index == pagesInfo.length - 1)
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushAndRemoveUntil(
-                      context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => const RootPage(),
-                      ),
-                      ((route) => false),
-                    );
-                  },
-                  child: const Text("Get Started!"),
-                ),
-            ],
+                const SizedBox(height: 64),
+                if (index == _pagesInfo.length - 1)
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => const RootPage(),
+                        ),
+                        ((route) => false),
+                      );
+                    },
+                    child: const Text("Get Started!"),
+                  ),
+              ],
+            ),
           ),
         ),
       ],
+    );
+  }
+
+  List<Widget> _indictators(BuildContext context) {
+    List<Widget> indicators = [];
+    for (int i = 0; i < _pagesInfo.length; i++) {
+      indicators.add(_indicator(context, i));
+    }
+    return indicators;
+  }
+
+  Widget _indicator(BuildContext context, int index) {
+    return AnimatedContainer(
+      height: 16,
+      width: 16,
+      duration: const Duration(milliseconds: 500),
+      decoration: BoxDecoration(
+        color: index == _activePage ? AppColors.primary : Colors.transparent,
+        borderRadius: BorderRadiusGeometry.circular(0),
+        border: .all(color: AppColors.primary, width: 0.4),
+      ),
     );
   }
 }
