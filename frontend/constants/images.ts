e2c8dac8e2a53fa4de397562ad4f55ee0693fca4
AmirAbidi7 <abidiamir7@gmail.com>
2026-09-11 22:42:41 +0100

    feat: added header, svg support to directly import

--- frontend/constants/images.ts
+++ frontend/constants/images.ts
@@ -1,3 +1,4 @@
 import onboarding from "@/assets/images/onboarding/onboarding1.jpg";
+import logo from "@/assets/vectors/logo.svg";
 
-export default onboarding;
+export default { onboarding, logo };
--- frontend/src/app/(main)/_layout.tsx
+++ frontend/src/app/(main)/_layout.tsx
@@ -1,15 +1,31 @@
+import Logo from "@/assets/vectors/logo.svg";
+import { clsx } from "clsx";
 import { Tabs } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { tabs } from "../../../constants/data";
 import { colors } from "../../../constants/theme";
-import { clsx } from "clsx";
-import { Text } from "react-native";
 
-const Header = () => {};
+const Header = () => {
+  return (
+    <View className="flex flex-row justify-between bg-background w-full px-2">
+      <Logo width={52} height={52} />
+      <View className="px-2 bg-green-500/20 rounded-2xl items-center justify-center my-3">
+        <Text className="text-emerald-500 text-center">LIVE! ●</Text>
+      </View>
+    </View>
+  );
+};
 export default function MainLayout() {
   return (
     <Tabs
       screenOptions={{
-        headerShown: false,
+        headerShown: true,
+        header: () => (
+          <SafeAreaView className="flex-1">
+            <Header />,
+          </SafeAreaView>
+        ),
         tabBarStyle: {
           backgroundColor: colors.background,
           marginVertical: 20,
