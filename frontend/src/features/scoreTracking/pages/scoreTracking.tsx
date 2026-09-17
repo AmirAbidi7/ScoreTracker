ece7899c6dbf51efa6bfd6ac8305ea72efead603
AmirAbidi7 <abidiamir7@gmail.com>
2026-09-17 19:54:12 +0100

    added ranking functionality that automatically updates & caches values with useMemo

--- frontend/src/features/scoreTracking/pages/scoreTracking.tsx
+++ frontend/src/features/scoreTracking/pages/scoreTracking.tsx
@@ -1,21 +1,25 @@
-import { useState } from "react";
-import { View, Text, TextInput, Pressable } from "react-native";
+import { useMemo, useState } from "react";
+import { Pressable, Text, TextInput, View } from "react-native";
 import { scores as sc } from "../../../../constants/data";
 import { Player } from "../domain/Scoreboard";
 
 const PlayerCard = ({
   player,
   updateScore,
+  ranking,
 }: {
   player: Player;
   updateScore: (playerId: number, amount: number) => void;
+  ranking: number;
 }) => {
   const [amount, setAmount] = useState("1");
   return (
     <View className="mx-8 border border-secondary flex items-stretch px-4 bg-black">
       <View className="flex justify-between items-center flex-row">
-        <Text className="text-white font-sans-medium text-2xl">Player:</Text>
-        <Text className="text-white font-sans-medium text-2xl">{player.name}</Text>
+        <View className="flex justify-center items-center flex-row gap-4">
+          <Text className="text-white font-sans-medium text-2xl">{ranking}.</Text>
+          <Text className="text-white font-sans-medium text-2xl">{player.name}</Text>
+        </View>
       </View>
       <View className="flex justify-between items-center flex-row gap-4">
         <View className="flex flex-row gap-2">
@@ -49,6 +53,7 @@ const PlayerCard = ({
 
 export default function ScoreTrackingPage() {
   const [scores, setScores] = useState(sc);
+  const rankings = useMemo(sortPlayerRankings, [scores]);
 
   function updateScore(playerId: number, amount: number) {
     setScores((currentScores) => ({
@@ -59,15 +64,29 @@ export default function ScoreTrackingPage() {
     }));
   }
 
+  function sortPlayerRankings() {
+    const sortedScores = [...scores.players].sort(
+      (player1, player2) => player2.score - player1.score,
+    );
+    const rankings: number[] = [];
+    scores.players.forEach((player) => {
+      const index = sortedScores.findIndex((playa) => player.id === playa.id);
+      rankings.push(index + 1);
+    });
+
+    return rankings;
+  }
+
   return (
-    <View className="bg-background flex-1 pt-12 gap-8">
+    <View className="bg-black flex-1 pt-12 gap-8">
       <View className="mx-8 px-4 border border-primary bg-black flex justify-between items-center flex-row">
         <Text className="text-white font-sans-medium text-2xl">Game: </Text>
         <Text className="text-white font-sans-medium text-2xl">{scores.gameName}</Text>
       </View>
-      {scores.players.map((currentPlayer) => (
+      {scores.players.map((currentPlayer, index) => (
         <PlayerCard
           key={currentPlayer.id}
+          ranking={rankings[index]}
           player={currentPlayer}
           updateScore={updateScore}
         ></PlayerCard>
