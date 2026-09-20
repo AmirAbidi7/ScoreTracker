ee49ca57c0e45466c9b1c314cfac96338bae3307
AmirAbidi7 <abidiamir7@gmail.com>
2026-09-20 18:14:10 +0100

    fix: bound backend tsconfig include/exclude to stop tsc full-disk scan

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -2,4 +2,13 @@ import { drizzle } from "drizzle-orm/node-postgres";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
-module.exports = db;
+export class Database extends Context.Service<Database, Db>()("Database", {
+  make: Effect.sync(() => drizzle(process.env.DATABASE_URL!)),
+}) {}
+
+export const DatabaseLive = Layer.effect(Database, Database.make);
+
+const program = Effect.gen(function* () {
+  const db = yield* Database;
+  db.query();
+});
